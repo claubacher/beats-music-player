ecdedd9cfbcabeb26db9abea403aa7421c312a97
Chris <claubacher@gmail.com>
2014-04-12 15:38:20 -0700

    Streams hardcoded track

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,8 +1,21 @@
-soundManager.setup({
-  url: 'vendor/SM2/swf',
-  flashVersion: 9,
-  preferFlash: false,
-  onready: function() {
-    console.log(accessToken);
-  }
+$(function() {
+  soundManager.setup({
+    url: '/vendor/SM2/swf/',
+    flashVersion: 9,
+    preferFlash: true,
+    onready: function() {
+      var url = 'https://partner.api.beatsmusic.com/v1/api/tracks/' + 
+          'tr51760477/' +
+          'audio?' +
+          'acquire=1' +
+          '&access_token=' + accessToken;
+
+      $.getJSON(url, function(res) {
+        soundManager.createSound({
+          url: res.data.resource,
+          serverURL: res.data.location,
+        }).play();
+      });
+    }
+  });
 });
