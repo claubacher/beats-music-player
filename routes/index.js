5bde1060967df10ddfbda2aaf5743c7c3ed61c6b
Chris <claubacher@gmail.com>
2014-04-12 13:46:50 -0700

    Use implicit flow for getting access token

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var qs = require('querystring');
+var baseBeatsUri = 'https://partner.api.beatsmusic.com/v1/oauth2/'
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var authUrl = baseBeatsUri + 'authorize?' + qs.stringify({
+    response_type: 'token',
+    redirect_uri: req.protocol + '://' + req.get('host') + '/play',
+    client_id: process.env.BEATS_KEY
+  });
+
+  res.render('index', { title: 'Log in', authUrl: authUrl });
+});
+
+router.get('/play', function(req, res) {
+  var accessToken = req.query.access_token;
+  res.render('player', { title: 'Beats Music Player', accessToken: accessToken });
 });
 
 module.exports = router;
