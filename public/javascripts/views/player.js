b6fed044cc706fdaf98149ac4eb18ebb4abd001d
Chris <claubacher@gmail.com>
2014-04-13 18:36:15 -0700

    Add skip forward and backward

--- public/javascripts/views/player.js
+++ public/javascripts/views/player.js
@@ -5,7 +5,7 @@ var Player = Backbone.View.extend({
   el: '.controls-container',
 
   events: {
-    'click .play-pause': 'togglePlaying',
+    'click .play-pause': 'playPause',
     'click .skip-backward': 'skipBackward',
     'click .skip-forward': 'skipForward',
     'change .volume': 'changeVolume'
@@ -26,20 +26,60 @@ var Player = Backbone.View.extend({
     $.getJSON(url, function(res) {
       this.sound = soundManager.createSound({
         url: res.data.resource,
-        serverURL: res.data.location
+        serverURL: res.data.location,
+        autoPlay: true
       });
     }.bind(this));
+
+    this.togglePlaying();
   },
 
   togglePlaying: function() {
     if (app.playing) {
-      this.sound.pause();
       app.playing = false;
     } else {
-      this.sound.play();
       app.playing = true;
     }
 
     this.$el.find('.play-pause').children().toggleClass('active');
+  },
+
+  playPause: function() {
+    if (app.playing) {
+      this.sound.pause();
+    } else {
+      this.sound.play();
+    }
+
+    this.togglePlaying();
+  },
+
+  stop: function() {
+    this.sound.stop();
+    this.togglePlaying();
+  },
+
+  skipBackward: function() {
+    var currentIdx = app.currentPlaylist.tracks.indexOf(app.currentTrack);
+    if (currentIdx > 0) {
+      this.sound.destruct();
+      this.togglePlaying();
+      app.currentTrack = app.currentPlaylist.tracks[currentIdx - 1];
+      this.render();
+    } else {
+      this.stop();
+    }
+  },
+
+  skipForward: function() {
+    var currentIdx = app.currentPlaylist.tracks.indexOf(app.currentTrack);
+    if (currentIdx < app.currentPlaylist.tracks.length - 1) {
+      this.sound.destruct();
+      this.togglePlaying();
+      app.currentTrack = app.currentPlaylist.tracks[currentIdx + 1];
+      this.render();
+    } else {
+      this.stop();
+    }
   }
 });
