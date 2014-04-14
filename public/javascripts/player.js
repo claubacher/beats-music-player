f248fa66be858301f399bf661b9514cd0a45d7d1
Chris <claubacher@gmail.com>
2014-04-13 17:46:07 -0700

    Add togglePlaying to player

--- public/javascripts/player.js
+++ public/javascripts/player.js
@@ -1,8 +1,3 @@
-var app = {
-  beatsBaseUrl: 'https://partner.api.beatsmusic.com/v1/api/',
-  clientId: 'rwrwapvr4jng2jvp6aeem5wa'
-};
-
 $(function() {
   soundManager.setup({
     url: '/vendor/SM2/swf/',
@@ -10,75 +5,21 @@ $(function() {
     preferFlash: true
   });
 
-  var featuredUrl = app.beatsBaseUrl + 'discoveries/featured?client_id=' + app.clientId;
-  var editorPicksUrl = app.beatsBaseUrl + 'discoveries/editor_picks?client_id=' + app.clientId;
+  soundManager.onready(function() {
+    var featuredUrl = app.beatsBaseUrl + 'discoveries/featured?client_id=' + app.clientId;
+    var editorPicksUrl = app.beatsBaseUrl + 'discoveries/editor_picks?client_id=' + app.clientId;
 
-  var featuredPlaylistsView = new FeaturedPlaylistsView([ featuredUrl, editorPicksUrl ]);
-  var playlistView = new PlaylistView();
-  var player = new Player();
+    var featuredPlaylistsView = new FeaturedPlaylistsView([ featuredUrl, editorPicksUrl ]);
+    var playlistView = new PlaylistView();
+    var player = new Player();
 
-  app.loadPlaylist = function(playlist) {
-    // console.dir(playlist);
-    // playlistView.loadPlaylist(playlist);
-    app.currentPlaylist = playlist;
-    app.currentTrack = playlist.tracks[0];
-    playlistView.render();
-    player.render();
-  };
-
-  // $.getJSON(, function(res) {
-    // res.data.forEach(function(data) {
-      // var playlist = new Playlist(data);
-      
-  //   getFeatured(res);
-
-  //   var defaultPlaylist = featured[0].content;
-
-  //   var name = defaultPlaylist.name ? defaultPlaylist.name : defaultPlaylist.title; 
-
-  //   $('.playlist-name').text(name);
-
-  //   defaultPlaylist.refs.tracks.forEach(function(track) {
-  //     $('.playlist').append($('<li>' + track.display + '</li>'));
-  //   });
-
-  //   var defaultTrack = defaultPlaylist.refs.tracks[0];
-
-  //   var url = beatsBaseUrl + 'tracks/' + 
-  //     defaultTrack.id + '/' +
-  //     'audio?' +
-  //     'acquire=1' +
-  //     '&access_token=' + accessToken;
-
-  //   $('.track-name').text(defaultTrack.display)
-
-  //   $.get(beatsBaseUrl + 'tracks/' + defaultTrack.id + '/artists?client_id=' + clientId, function(res) {
-  //     $('.artist-name').text(res.data[0].name);
-  //   });
-
-  //   soundManager.onready(function() {
-  //     var playing = false;
-
-  //     $.getJSON(url, function(res) {
-  //       var sound = soundManager.createSound({
-  //         url: res.data.resource,
-  //         serverURL: res.data.location
-  //       });
-
-  //       $('.play-pause').on('click', function() {
-  //         if (playing) {
-  //           sound.pause();
-  //           playing = false;
-  //         } else {
-  //           sound.play();
-  //           playing = true;
-  //         }
-
-  //         $(this).children().toggleClass('active');
-  //       });
-  //     });
-  //   });
-  // });
-
-  // $.getJSON(, getFeatured);
+    app.loadPlaylist = function(playlist) {
+      app.currentPlaylist = playlist;
+      app.currentTrack = playlist.tracks[0];
+      playlistView.render();
+      if (!app.playing) {
+        player.render();
+      }
+    };
+  });
 });
--- public/javascripts/views/player.js
+++ public/javascripts/views/player.js
@@ -16,10 +16,30 @@ var Player = Backbone.View.extend({
       $('.track-name').text(app.currentTrack.name);
       $('.artist-name').text(res.data[0].name);
     });
+
+    this.getAudio();
   },
 
-  playing: false,
+  getAudio: function() {
+    var url = app.beatsBaseUrl + 'tracks/' + app.currentTrack.id + '/' + 'audio?' + 'acquire=1' + '&access_token=' + app.accessToken;
+
+    $.getJSON(url, function(res) {
+      this.sound = soundManager.createSound({
+        url: res.data.resource,
+        serverURL: res.data.location
+      });
+    }.bind(this));
+  },
 
   togglePlaying: function() {
+    if (app.playing) {
+      this.sound.pause();
+      app.playing = false;
+    } else {
+      this.sound.play();
+      app.playing = true;
+    }
+
+    this.$el.find('.play-pause').children().toggleClass('active');
   }
 });
